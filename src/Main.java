48d44160a0209b2fe05c8b9b45fabbdfbc5be7bc
Erodar <stijnrelou@gmail.com>
2023-05-15 13:24:16 +0200

    changed to class

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        IntroductieClass tijdelijk = new IntroductieClass( "Blob");
+        IntroductieClass tijdelijk2 = new IntroductieClass( "Blab");
 
     }
 
