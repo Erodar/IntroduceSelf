c20248d126e3d243793c142e9bf29a10eedf54ac
Erodar <stijnrelou@gmail.com>
2023-05-15 11:45:53 +0200

    enters

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Hallo, ik ben Chris en dit is hoe ik mezelf voorstel");
         System.out.println("Ik ben 30. Ik woon in Voorburg, vlak bij station laan v NOI.");
         System.out.print("Ik heet eigenlijk Stijn, maar omdat er bij deze cursus 3 mensen ");
         System.out.println("diezelfde naam hebben gebruik ik Chris als roepnaam");
         System.out.println("Mijn voornaamste hobbies zijn wielrennen, gamen, lezen en koken.");
         System.out.println("Tot zovere mijn introductie.");
+
     }
+
 }
